3c5f6744ab68bc5870742a5e6ec2ca0087e7a86f
Haijie95 <haijie95@gmail.com>
2023-01-04 16:25:00 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# workshop 12
+
+Methods used
+
+1. Dynamic Content
+2. Thymeleaf-Iteration
+3. Methods, resources, status
+4. GET method query string
+
---
+++ src/main/java/com/example/workshop12/models/Generate.java
@@ -0,0 +1,16 @@
+package com.example.workshop12.models;
+
+//this is to store the number of random numbers to be generated
+public class Generate {
+    private int numberVal;
+
+    public int getNumberVal() {
+        return numberVal;
+    }
+
+    public void setNumberVal(int numberVal) {
+        this.numberVal = numberVal;
+    }
+
+    
+}
